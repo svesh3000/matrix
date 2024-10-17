b39ba83beee2d6a0b83b36371f87a6dd85d2acfa
svesh3000 <russkij2006@mail.ru>
2024-10-17 11:30:34 +0300

    main with mtx.hpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include "mtx.hpp"
 
 int main()
 {
   size_t m = 0, n = 0;
   std::cin >> m >> n;
   std::cout << m << " " << n << "\n";
+  int ** t = ct(m,n);
+  read(t, m, n);
+  write(t, m, n)
+  dt(t, m, n);
 }
