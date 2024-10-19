ac01a810f027918eed46ec5e1aadb9e5bd417414
svesh3000 <russkij2006@mail.ru>
2024-10-20 00:13:28 +0300

    Function memory_alloc

--- main.cpp
+++ main.cpp
@@ -8,9 +8,10 @@ int main()
   std::cin >> m >> n;
   if (!std::cin) {
     std::cerr << "ERROR\n";
+    return 1;
   }
   try {
-    int ** t = create(m,n);
+    int ** t = memory_alloc(m,n);
   }
   catch (const std::bad_alloc& e) {
     std::cerr << "Memory Allocation Error: " << e.what() << "\n";
