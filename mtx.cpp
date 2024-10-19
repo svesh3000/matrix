9b4b7c183537db6bb5069590942b31a987f6f277
svesh3000 <russkij2006@mail.ru>
2024-10-20 00:16:05 +0300

    Function read

--- mtx.cpp
+++ mtx.cpp
@@ -1,7 +1,35 @@
 #include "mtx.hpp"
-void clean (int ** t, size_t m)
+int ** memory_alloc(size_t m, size_t n)
 {
-  for (size_t i = 0; i < m; ++i){
+  int ** mtx = new int*[m];
+  for (size_t i = 0; i < m; i++) {
+    mtx[i] = new int[n];
+  }
+  return mtx;
+}
+
+void read(int ** mtx, size_t m, size_t n)
+{
+  int number = 0;
+  for (size_t i = 0; i < m; i++) {
+    for (size_t j = 0; j < n; i++) {
+      std::cin >> number;
+      mtx[i][j] = number;
+    }
+  }
+  if (!std::cin) {
+    std::cerr << "ERROR\n";
+  }
+}
+
+void write(const int * const * mtx, size_t m, size_t n)
+{
+
+}
+
+void clean(int ** t, size_t m)
+{
+  for (size_t i = 0; i < m; ++i) {
     delete [] t[i];
   }
   delete[] t;
